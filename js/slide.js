11f5c3190dd21d00cc06f96f6e7ca0769e985b51
joaoKremerDev <joaokremer.dev@gmail.com>
2023-01-31 14:50:59 -0300

    touch events

--- js/slide.js
+++ js/slide.js
@@ -16,17 +16,28 @@ export default class Slide {
     }
 
     onStart(event) {
-        event.preventDefault();
-        this.wrapper.addEventListener('mousemove', this.onMove)
+        let moveType;
+        if (event.type === 'mousedown') {
+            event.preventDefault();
+            this.dist.startX = event.clientX;
+            moveType = 'mousemove';
+        } else {
+            this.dist.startX = event.changedTouches[0].clientX;
+            moveType = 'touchmove';
+        }
+        this.wrapper.addEventListener(moveType, this.onMove)
     }
 
     onMove(event) {
-        
+        const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
+        const finalPosition = this.updatePosition(pointerPosition);
+        this.moveSlide(finalPosition);
     }
 
-    onEnd(event){
-        console.log('acabou')
-        this.wrapper.removeEventListener('mousemove', this.onMove)
+    onEnd(event) {
+        const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
+        this.wrapper.removeEventListener(movetype, this.onMove);
+        this.dist.finalPosition = this.dist.movePosition;
     }
 
     bindEvents() {
